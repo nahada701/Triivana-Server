2f495585ea7c1922f7e78564e010f2fabd210af4
Nahada C <nahadac701@gmail.com>
2025-03-15 19:24:22 +0530

    connection string changed

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const connection_string=process.env.connection_string
+const connection_string=process.env.CONNECTION_STRING
 
 mongoose.connect(connection_string).then(res=>{
     console.log("mongodb connected to server sucessfully");
